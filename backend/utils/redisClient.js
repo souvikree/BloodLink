46deb9decbb0274e8d00e09393aa33c8fb3e0902
souvikree <souvikg3225@gmail.com>
2025-04-12 23:54:56 +0530

    new update: redis connect to Render

--- backend/utils/redisClient.js
+++ backend/utils/redisClient.js
@@ -1,13 +1,14 @@
 const Redis = require('ioredis');
 
-//use redis container
-const redis = new Redis(process.env.REDIS_URL || {
-  host: process.env.REDIS_HOST || '127.0.0.1',
-  port: process.env.REDIS_PORT || 6379,
-});
+// Use the full Redis URL if available (for both local and Render)
+const redis = process.env.REDIS_URL 
+  ? new Redis(process.env.REDIS_URL) 
+  : new Redis({
+      host: process.env.REDIS_HOST || '127.0.0.1', // Use Redis container name or localhost for local development
+      port: process.env.REDIS_PORT || 6379, // Default Redis port
+    });
 
 redis.on('connect', () => console.log('✅ Redis connected'));
 redis.on('error', (err) => console.error('❌ Redis error:', err));
 
 module.exports = redis;
-
