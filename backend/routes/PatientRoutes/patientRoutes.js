2e53665592fe33b57675979ee7730f5ef7cb4915
souvikree <souvikg3225@gmail.com>
2025-04-12 20:38:55 +0530

    new update: patient registration workflow changed

--- backend/routes/PatientRoutes/patientRoutes.js
+++ backend/routes/PatientRoutes/patientRoutes.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const {
   registerPatient,
-  loginPatient,
+  verifyOtpForRegistration,
+  addPatientDetails,
+  loginSendOtp,
+  loginVerifyOtp,
   getProfile,
   updateProfile,
   searchNearby,
 } = require('../../controllers/PatientController/patientController.js');
+
+// const { sendOtpToMobile, verifyOtp } = require('../../controllers/PatientController/otpController.js');
 const {
   placeOrder,
   getOrderHistory,
@@ -21,12 +26,14 @@ const { uploadPrescription } = require("../../middleware/BloodBankMiddleware/upl
 const router = express.Router();
 
 router.post('/signup', registerPatient);
-router.post('/login', loginPatient);
+router.post('/verify-registration', verifyOtpForRegistration);
+router.put('/complete-profile', protect, addPatientDetails);
 
-router.post('/add-address', protect, addAddressAfterRegistration);
+router.post('/login/send-otp', loginSendOtp);
+router.post('/login/verify-otp', loginVerifyOtp);
+
 
 router.get('/profile', protect, getProfile);
-router.put('/profile', protect, updateProfile);
 
 router.get('/search', protect, searchNearby);
 router.get('/search-banks', searchBloodBanks);
@@ -34,3 +41,6 @@ router.post('/place-order', protect, uploadPrescription.single('prescription'), 
 router.get('/orders/history', protect, getOrderHistory);
 
 module.exports = router;
+
+// router.post('/add-address', protect, addAddressAfterRegistration);
+// router.put('/profile', protect, updateProfile);
