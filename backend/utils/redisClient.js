5613f1ecfd2cf7ed1535d3e517b2e299a26f37bb
souvikree <souvikg3225@gmail.com>
2025-04-12 23:26:48 +0530

    new update: redis server issue resolve

--- backend/utils/redisClient.js
+++ backend/utils/redisClient.js
@@ -2,7 +2,7 @@ const Redis = require('ioredis');
 
 //use redis container
 const redis = new Redis(process.env.REDIS_URL || {
-  host: process.env.REDIS_HOST || 'redis',
+  host: process.env.REDIS_HOST || '127.0.0.1',
   port: process.env.REDIS_PORT || 6379,
 });
 
