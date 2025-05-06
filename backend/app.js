7bfcc3b053464bbe419312b3321b3719ba846d79
souvikree <souvikg3225@gmail.com>
2025-05-06 21:39:58 +0530

    Update: app change 2

--- backend/app.js
+++ backend/app.js
@@ -43,7 +43,7 @@ app.use(cors({
 
 // ✅ ⬇️ Add this CORS fallback middleware immediately after
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+    res.header("Access-Control-Allow-Origin", "http://localhost:8080", "https://bloodlink-dashboard.vercel.app","http://localhost:8081");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.header("Access-Control-Allow-Credentials", "true");
